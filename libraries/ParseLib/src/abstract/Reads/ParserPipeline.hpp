21fbfc6bfa7339947dbeb8a27e168156c89601c0
Lucas Sorenson <lsorenson20@gmail.com>
2019-08-26 21:05:24 -0500

    just some formating changes

--- libraries/ParseLib/src/abstract/Reads/ParserPipeline.hpp
+++ libraries/ParseLib/src/abstract/Reads/ParserPipeline.hpp
@@ -19,10 +19,26 @@ class ParserPipeline
 
     //pipeline steps
     
-    int32_t ApplyFilters(plDataSet &data_set, plNodeBuffer &out_buffer, plNodeBuffer &in_buffer);
-    int32_t ProcessNodeSets(plNodeBuffer &out_buffer, plNodeBuffer &in_buffer, std::shared_ptr<ParserFilter> filter);
-    int32_t ProcessNodes(plNodeBuffer &out_buffer, plNodeSet &in_buffer, std::shared_ptr<ParserFilter> filter);
-    int32_t ProcessIndividual(plNodeSet &out_buffer, std::vector<node> &in_buffer, plNodePtr &current_node, std::shared_ptr<ParserFilter> filter);
+    int32_t ApplyFilters(
+        plDataSet &data_set, 
+        plNodeBuffer &out_buffer, 
+        plNodeBuffer &in_buffer);
+
+    int32_t ProcessNodeSets(
+        plNodeBuffer &out_buffer, 
+        plNodeBuffer &in_buffer, 
+        std::shared_ptr<ParserFilter> filter);
+
+    int32_t ProcessNodes(
+        plNodeBuffer &out_buffer, 
+        plNodeSet &in_buffer, 
+        std::shared_ptr<ParserFilter> filter);
+        
+    int32_t ProcessIndividual(
+        plNodeSet &out_buffer, 
+        std::vector<node> &in_buffer, 
+        plNodePtr &current_node, 
+        std::shared_ptr<ParserFilter> filter);
     
 public:
     ParserPipeline() = default;
