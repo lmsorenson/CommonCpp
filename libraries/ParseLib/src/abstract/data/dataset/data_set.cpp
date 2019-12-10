a9862a003e85508136253040dc5027c81662dc93
Lucas Sorenson <lsorenson20@gmail.com>
2019-12-09 22:06:05 -0600

    add read and write calls to the data set

--- libraries/ParseLib/src/abstract/data/dataset/data_set.cpp
+++ libraries/ParseLib/src/abstract/data/dataset/data_set.cpp
@@ -2,6 +2,8 @@
 #include <objects/data_set.hpp>
 #include <iostream>
 
+#include <ParseLib.hpp>
+
 
 
 
@@ -28,4 +30,43 @@ sdg::DataSet::~DataSet(){}
 sdg::Instance sdg::DataSet::operator[](std::string i)
 {
     return this->get(i);
+}
+
+
+
+sdg::DataSet sdg::DataSet::Read(std::string a_path, int32_t * status_code)
+{
+    int32_t return_code;
+    return_code = ParseLib().read_file(*this, a_path.c_str());
+    if(status_code!=nullptr)
+        *status_code = return_code;
+
+    return *this;
+}
+
+sdg::DataSet sdg::DataSet::Read(std::string a_path, std::vector<option> read_options, int32_t * status_code)
+{
+    int32_t return_code;
+    return_code = ParseLib().read_file(*this, a_path.c_str(), read_options);
+    if(status_code!=nullptr)
+        *status_code = return_code;
+
+    return *this;
+}
+
+void sdg::DataSet::Write(std::string a_path, int32_t * status_code)
+{
+    int32_t return_code;
+    return_code = ParseLib().write_file(*this, a_path.c_str());
+    if(status_code!=nullptr)
+        *status_code = return_code;
+
+}
+
+void sdg::DataSet::Write(std::string a_path, std::vector<option> write_options, int32_t * status_code)
+{
+    int32_t return_code;
+    return_code = ParseLib().write_file(*this, a_path.c_str(), write_options);
+    if(status_code!=nullptr)
+        *status_code = return_code;
 }
