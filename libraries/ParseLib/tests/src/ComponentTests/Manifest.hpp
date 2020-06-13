637ef054ce464b67a437fa3a9767d36a48a1aa76
Lucas Sorenson <lsorenson20@gmail.com>
2020-06-13 09:09:14 -0500

    add some tests

--- libraries/ParseLib/tests/src/ComponentTests/Manifest.hpp
+++ libraries/ParseLib/tests/src/ComponentTests/Manifest.hpp
@@ -1,3 +1,5 @@
 #pragma once
 #include "ComponentTests.hpp"
 #include "LexerComponent/Tests.hpp"
+#include "ParserComponentTests/Tests.hpp"
+#include "ParserComponentTests/SequenceTests/SequenceTests.hpp"
---
+++ libraries/ParseLib/tests/src/ComponentTests/ParserComponentTests/SequenceTests/TestTypes.hpp
@@ -0,0 +1,60 @@
+#pragma once
+#include "../../../../../src/abstract/parser/public/IndependentEntity.hpp"
+
+
+
+class TestTokenA : public sdg::parse::TokenType, public sdg::parse::IndependentEntity
+{
+public:
+    TestTokenA() 
+    : TokenType("A", Cardinality::One)
+    , sdg::parse::IndependentEntity()
+    {}
+    ~TestTokenA() = default;
+    
+    virtual bool classify(std::string a_token) const override
+    {
+        if (a_token.compare("A") == 0)
+            return true;
+        else
+            return false;
+    }
+
+    virtual std::pair<std::string, std::string> create_node(std::string a_token) override
+    {
+        return std::pair<std::string, std::string>("A", "A");
+    }
+
+    virtual std::string get_id() const override
+    {
+        return string("A" + std::to_string(shape_occurances_-1));
+    }
+};
+
+class TestTokenB : public sdg::parse::TokenType, public sdg::parse::IndependentEntity
+{
+public:
+    TestTokenB() 
+    : TokenType("B", Cardinality::One)
+    , sdg::parse::IndependentEntity()
+    {}
+    ~TestTokenB() = default;
+    
+    virtual bool classify(std::string a_token) const override
+    {
+        if (a_token.compare("B") == 0)
+            return true;
+        else
+            return false;
+    }
+
+    virtual std::pair<std::string, std::string> create_node(std::string a_token) override
+    {
+        return std::pair<std::string, std::string>("A", "A");
+    }
+
+    virtual std::string get_id() const override
+    {
+        return string("B" + std::to_string(shape_occurances_-1));
+    }
+};
