d0f366938a8e87bea48be115c33f38320fdb1cdb
Lucas Sorenson <lsorenson20@gmail.com>
2020-06-26 20:44:38 -0500

    adjust tests

--- libraries/ParseLib/tests/src/ComponentTests/LexerComponent/ShapeTests/StateTests.hpp
+++ libraries/ParseLib/tests/src/ComponentTests/LexerComponent/ShapeTests/StateTests.hpp
@@ -16,17 +16,16 @@ TEST_F(LexerComponentTests, shape_run_ )
     Lexer lexer = Lexer();
     ShapeA shape(&lexer, ShapeA::Cardinality::One);
     shape.add_state<MockStateA>();
+    auto mock_state_a = std::dynamic_pointer_cast<MockStateA>(shape.get_state<MockStateA>()).get();
 
-    EXPECT_CALL(*(std::dynamic_pointer_cast<MockStateA>(shape.get_state<MockStateA>()).get()), initialize('\0'))
+    EXPECT_CALL(*mock_state_a, initialize('\0'))
     .Times(testing::Exactly(1));
 
-    EXPECT_CALL(*(std::dynamic_pointer_cast<MockStateA>(shape.get_state<MockStateA>()).get()), perform_scan('A'))
+    EXPECT_CALL(*mock_state_a, perform_scan('A'))
     .Times(testing::Exactly(1));
 
     bool should_buffer;
-
     shape.run(should_buffer, 'A');
-    
 }
 
 TEST_F(LexerComponentTests, shape_run_with_no_states_does_not_fail )
