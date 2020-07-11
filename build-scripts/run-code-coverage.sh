ee8baee8fba133983dea02fc300b7cf842d8dc69
Lucas Sorenson <lsorenson20@gmail.com>
2020-07-11 11:34:08 -0500

    modify the run_code_coverage

--- build-scripts/run-code-coverage.sh
+++ build-scripts/run-code-coverage.sh
@@ -6,7 +6,9 @@ cd CMakeFiles/TestComponents.dir/src/
 
 pwd
 ls
-gcov-9 -version
-gcov-9 TestComponents.gnco
+gcov -version
+gcov TestComponents.gnco
 lcov --capture --directory . --output-file PROJECT_NAME.info
 genhtml PROJECT_NAME.info --output-directory CODE_COVERAGE
+cd CODE_COVERAGE/
+open index.html
