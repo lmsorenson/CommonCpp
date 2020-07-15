4eb02595a24dba1d0a53bf8dd0d270657727516b
Lucas Sorenson <lsorenson20@gmail.com>
2020-07-14 22:38:16 -0500

    structure components with their own CMakeLists.txt

--- libraries/ParseLib/src/abstract/semantic_analyzer/private/source/DataSetTarget.cpp
+++ libraries/ParseLib/src/abstract/semantic_analyzer/private/source/DataSetTarget.cpp
@@ -1,5 +1,5 @@
 // Copyright 2020, Lucas Sorenson, All rights reserved.
-#include "../private/DataSetTarget.hpp"
+#include "../../private/DataSetTarget.hpp"
 #include <iostream>
 #include <vector>
 
