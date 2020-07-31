c7bb3791c01a1e93bbd4c0176b31d91a29512daa
Lucas Sorenson <lsorenson20@gmail.com>
2020-07-30 19:20:24 -0500

    adjust test framework

--- libraries/ParseLib/tests/src/TestFramework.hpp
+++ libraries/ParseLib/tests/src/TestFramework.hpp
@@ -5,18 +5,19 @@
 class LucTestFramework : public ::testing::Test
 {
 public:
-    static void SetPath(std::string new_path)
+    static void SetPath(const std::string &new_path)
     {
         root = new_path;
     }
 
-    static std::string path(std::string relative_path)
+    static std::string path(const std::string &relative_path)
     {
         std::string path_buffer = root;
         path_buffer.append(relative_path);
         std::cout << "Path used: " << path_buffer << std::endl;
         return path_buffer;
     }
+
 protected:
     static std::string root;
 
