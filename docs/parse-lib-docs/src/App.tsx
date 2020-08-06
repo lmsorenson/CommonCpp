905c5d7d463308e085738dcb52f8ca9c357096b5
Lucas Sorenson <lsorenson20@gmail.com>
2020-08-05 22:42:17 -0500

    update the website

--- docs/parse-lib-docs/src/App.tsx
+++ docs/parse-lib-docs/src/App.tsx
@@ -2,17 +2,29 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          CommonCpp page
-        </p>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  
+  constructor(props: any)
+  {
+    super(props);
+
+    document.title = 'PL | Documentation'
+  }
+
+  public render()
+  {
+    return (
+        <div className="App">
+          <title>Page</title>
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              CommonCpp page
+            </p>
+          </header>
+        </div>
+    );
+  }
 }
 
 export default App;
