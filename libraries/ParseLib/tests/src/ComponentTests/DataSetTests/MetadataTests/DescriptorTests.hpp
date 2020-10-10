7a551efb161df69b4ca848000a2a6f9ae9fc59c1
Lucas Sorenson <lsorenson20@gmail.com>
2020-10-09 22:56:19 -0500

    fix link tests

--- libraries/ParseLib/tests/src/ComponentTests/DataSetTests/MetadataTests/DescriptorTests.hpp
+++ libraries/ParseLib/tests/src/ComponentTests/DataSetTests/MetadataTests/DescriptorTests.hpp
@@ -69,20 +69,21 @@ TEST_F(MetadataDescriptorTests, link_get_identifying_descriptor_ids )
 TEST_F(MetadataDescriptorTests, link_get_descriptor_ids )
 {
     ::std::shared_ptr<::sdg::Entity> entityA = ::std::make_shared<::sdg::Entity>("A", "entity a");
-    ::std::shared_ptr<::sdg::Attribute> a_id_attribute;
+    ::std::shared_ptr<::sdg::Attribute> a_id_1_attribute;
+    ::std::shared_ptr<::sdg::Attribute> a_id_2_attribute;
+    ::std::shared_ptr<::sdg::Attribute> a_desc_3_attribute;
 
-    entityA->add_descriptor(a_id_attribute=::std::make_shared<::sdg::Attribute>(::sdg::Attribute("a_id_1", "A1", ::sdg::Attribute::Scale::Ordinal)), true);
-    entityA->add_descriptor(a_id_attribute=::std::make_shared<::sdg::Attribute>(::sdg::Attribute("a_id_2", "A2", ::sdg::Attribute::Scale::Ordinal)), true);
-    entityA->add_descriptor(a_id_attribute=::std::make_shared<::sdg::Attribute>(::sdg::Attribute("a_desc_3", "A3", ::sdg::Attribute::Scale::Ordinal)), false);
+    entityA->add_descriptor(a_id_1_attribute=::std::make_shared<::sdg::Attribute>(::sdg::Attribute("a_id_1", "A1", ::sdg::Attribute::Scale::Ordinal)), true);
+    entityA->add_descriptor(a_id_2_attribute=::std::make_shared<::sdg::Attribute>(::sdg::Attribute("a_id_2", "A2", ::sdg::Attribute::Scale::Ordinal)), true);
+    entityA->add_descriptor(a_desc_3_attribute=::std::make_shared<::sdg::Attribute>(::sdg::Attribute("a_desc_3", "A3", ::sdg::Attribute::Scale::Ordinal)), false);
 
     ::std::shared_ptr<::sdg::Link> link = ::std::make_shared<::sdg::Link>("link", entityA);
 
     ::std::vector<::std::shared_ptr<::sdg::Descriptor>> actual_identifier = link->get_descriptors();
     ::std::vector<::std::shared_ptr<::sdg::Descriptor>> expected_identifier
     {
-
-        ::std::make_shared<::sdg::Attribute>(::sdg::Attribute("a_id_1", "A1", ::sdg::Attribute::Scale::Ordinal)),
-        ::std::make_shared<::sdg::Attribute>(::sdg::Attribute("a_id_2", "A2", ::sdg::Attribute::Scale::Ordinal))
+        a_id_1_attribute,
+        a_id_2_attribute
     };
 
     ASSERT_THAT(actual_identifier, ::testing::ContainerEq(expected_identifier));
