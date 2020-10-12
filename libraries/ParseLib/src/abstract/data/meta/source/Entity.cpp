52e1a9e78ceda300d9b40522435e2655fa4f4660
Lucas Sorenson <lsorenson20@gmail.com>
2020-10-11 21:38:03 -0500

    add entity coverage

--- libraries/ParseLib/src/abstract/data/meta/source/Entity.cpp
+++ libraries/ParseLib/src/abstract/data/meta/source/Entity.cpp
@@ -17,6 +17,11 @@ using ::std::vector;
 using ::std::cout;
 using ::std::endl;
 
+/**
+ * The constructor for an Entity.
+ * @param a_entity_id A unique identifier for an entity.  Within a model no two entities can contain have the same entity id.
+ * @param a_name A name for the thing.
+ */
 Entity::Entity(const string &a_entity_id, const string &a_name)
 : Thing(a_name, a_entity_id )
 , identifier_array_(0, nullptr )
@@ -26,7 +31,12 @@ Entity::Entity(const string &a_entity_id, const string &a_name)
     identifier_array_.push_back(id);
 }
 
-//default param for identifier index is 0
+/**
+ * Add a single descriptor to the entity.
+ * @param a_descriptor The descriptor to add.
+ * @param b_is_identifying_descriptor Indicates whether or not the current descriptor is an identifying descriptor.
+ * @param identifier_index The identifier to add the descriptor to.
+ */
 void Entity::add_descriptor(shared_ptr<Descriptor> a_descriptor, bool b_is_identifying_descriptor, int32_t identifier_index)
 {
     descriptor_array_.push_back(a_descriptor);
@@ -45,6 +55,10 @@ void Entity::add_descriptor(shared_ptr<Descriptor> a_descriptor, bool b_is_ident
     }
 }
 
+/**
+ *
+ * @return
+ */
 vector<sdg::hash::DescriptorID> Entity::get_identifying_descriptor_id_set()
 {
     vector<hash::DescriptorID> result;
@@ -57,6 +71,10 @@ vector<sdg::hash::DescriptorID> Entity::get_identifying_descriptor_id_set()
 }
 
 
+/**
+ *
+ * @return
+ */
 vector<shared_ptr<Descriptor>> Entity::get_identifying_descriptor_set()
 {
     vector<shared_ptr<Descriptor>> result;
@@ -68,11 +86,20 @@ vector<shared_ptr<Descriptor>> Entity::get_identifying_descriptor_set()
     return result;
 }
 
+/**
+ * Number of descriptors.
+ * @return The total number of descriptors associated with this entity.
+ */
 size_t Entity::num_descriptors() const
 {
     return descriptor_array_.size();
 }
 
+/**
+ * Gets the size of the target identifier.
+ * @param a_identifier_index The target identifier.
+ * @return The size of the target identifier.  Returns 0 if the target identifier doesn't exist.
+ */
 size_t Entity::num_identifying_descriptors(int32_t a_identifier_index) const
 {
     if (identifier_array_[a_identifier_index] != nullptr)
@@ -84,17 +111,29 @@ size_t Entity::num_identifying_descriptors(int32_t a_identifier_index) const
 
 
 
-
+/**
+ * A constructor for an Identifier.
+ * @param a_owner A pointer to the owner of this identifier.
+ */
 Identifier::Identifier(Entity * a_owner)
 : owning_entity_(a_owner)
 {
 }
 
+/**
+ * Add a descriptor reference to the identifier.
+ * @param a_descriptor A descriptor pointer to add.
+ */
 void Identifier::add_descriptor(shared_ptr<Descriptor> a_descriptor)
 {
     this->descriptor_array_.push_back(a_descriptor);
 }
 
+
+/**
+ * Gets a list of descriptor ids.
+ * @return A vector of descriptor ids.
+ */
 vector<sdg::hash::DescriptorID> Identifier::get_descriptor_ids()
 {
     vector<hash::DescriptorID> result;
@@ -118,6 +157,10 @@ vector<sdg::hash::DescriptorID> Identifier::get_descriptor_ids()
     return result;
 }
 
+/**
+ * Get a list of descriptors associated with this identifier.
+ * @return A vector of descriptors associated with this identifier.
+ */
 vector<std::shared_ptr<Descriptor>> Identifier::get_descriptors()
 {
     vector<std::shared_ptr<Descriptor>> result;
@@ -139,6 +182,10 @@ vector<std::shared_ptr<Descriptor>> Identifier::get_descriptors()
     return result;
 }
 
+/**
+ * Get the number of descriptors associated with this identifier.
+ * @return The size of the descriptor array.
+ */
 size_t Identifier::num_descriptors() const
 {
     return descriptor_array_.size();
